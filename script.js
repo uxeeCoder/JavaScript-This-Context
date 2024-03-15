898224e7f076cfeb2d2959020813262680a2ef44
uxeeCoder <umar.afar@aol.com>
2024-03-15 16:06:50 -0400

    Created standard and arrow functions to see their behavior with 'this'

--- script.js
+++ script.js
@@ -41,7 +41,30 @@ button3.addEventListener(
 const person = {
   name:'Jenny',
 
-  greet: function () {
+  standardFunctionToGreet: function () {
     console.log ('Hello,', this.name);
   },
+
+  arrowFunctionToGreet : () => {
+    console.log ('Hello,', this.name);
+  }
 };
+
+// Invoking functions
+console.log('Standard Greeting');
+person.standardFunctionToGreet(); // Logs as Hello, Jenny
+
+// Explanation of 'this' in standard function:
+// In the standard function context, 'this' refers to the object 'person' since it's called as a method of 'person'. Therefore, it correctly logs "Hello, Frank".
+
+
+console.log('Arrow Function Greeting');
+person.arrowFunctionToGreet(); // Logs as Hello, undefined
+
+// Explanation of 'this' in arrow function:
+// In the arrow function context, 'this' retains the value of the enclosing context, which is the global scope. Hence, 'this.name' is undefined, resulting in "Hello, undefined".
+
+
+// Explanation of .bind():
+// In event listeners, .bind() is necessary to explicitly set the 'this' context to the 'messageHandler' object. Otherwise, 'this' would refer to the button element that triggered the event.
+// - This ensures that the methods like displayMessage1, displayMessage2, and displayMessage3 operate within the context of the 'messageHandler' object, allowing them to access properties like 'message1', 'message2', and 'message3' correctly.
